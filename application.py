3d460c0a52e37b212792ec3011bd7ed598ac13a6
faseem619 <84857173+faseem619@users.noreply.github.com>
2021-06-22 21:32:20 +0530

    Initial Comitt

---
+++ application.py
@@ -0,0 +1,20 @@
+from server_actions import addpost
+from flask import Flask,render_template,request
+from server_actions import addpost,getposts
+app =Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/home',methods=['POST'])
+def home():
+    name =request.form.get('name')
+    content =request.form.get('content')
+    addpost(name=name,content=content)
+    posts =getposts()
+    return render_template('home.html',posts=posts)
+
+if __name__ =="__main__":
+    app.run(debug=True)
