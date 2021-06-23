f30b14c6876107fb6dda6c02298273909cfca552
faseem619 <84857173+faseem619@users.noreply.github.com>
2021-06-23 19:45:32 +0530

    added post_date column

--- sqlite_create.py
+++ sqlite_create.py
@@ -3,6 +3,6 @@ import sqlite3
 
 con = sqlite3.connect('mydatabase.db')
 mycursor = con.cursor()
-mycursor.execute("CREATE TABLE posts(id INTEGER PRIMARY KEY autoincrement, name text NOT NULL,content text NOT NULL);")
+mycursor.execute("CREATE TABLE posts(id INTEGER PRIMARY KEY autoincrement, name text NOT NULL,content text NOT NULL,post_date text not null);")
 con.commit()
 con.close()
