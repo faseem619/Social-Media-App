71a794eb75cd155a05d1d7226e1c6ab463cd766c
faseem619 <84857173+faseem619@users.noreply.github.com>
2021-06-23 19:43:46 +0530

    added date mechanism

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from server_actions import addpost
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 from server_actions import addpost,getposts
+from datetime import datetime
 app =Flask(__name__)
 
 @app.route('/')
@@ -12,7 +13,10 @@ def index():
 def home():
     name =request.form.get('name')
     content =request.form.get('content')
-    addpost(name=name,content=content)
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+   
+    addpost(name=name,content=content,date1=dt_string)
     posts =getposts()
     return render_template('home.html',posts=posts)
 
