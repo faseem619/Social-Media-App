87271fcb1bfb17cc7518c36223ef6e71f1d4ee5a
Faseem Shanavas <faseem619@gmail.com>
2021-06-29 17:07:43 +0530

    new date format

--- static/home.js
+++ static/home.js
@@ -9,17 +9,12 @@ function handleMenu() {
     d.style.transform = "rotate(0deg)";
     d.setAttribute("clicked", "true");
     s.style.animation = "ani2 1s ease-in-out";
-    setTimeout(()=>{
+    setTimeout(() => {
       s.classList.remove("menu_active");
-      
-    },1000)
-    setTimeout(()=>{
-     
+    }, 1000);
+    setTimeout(() => {
       s.style.removeProperty("animation");
-      
-    },1000)
-    
-    
+    }, 1000);
   }
 }
 
@@ -64,6 +59,7 @@ window.addEventListener("scroll", () => {
 });
 
 window.onload = () => {
+  console.log("test");
   if (document.cookie.length != 0) {
     d = document.getElementById("conditional_icon");
     let user = getCookie("username");
@@ -74,6 +70,42 @@ window.onload = () => {
         '"><span class="material-icons-outlined"> account_circle </span></a>';
     }
   }
+  /////////////////////
+  const dates = document.querySelectorAll(".date");
+  const d1 = new Date();
+  const year = d1.getFullYear(),
+    month = d1.getMonth() + 1,
+    day = d1.getDate(),
+    hour = d1.getHours(),
+    minute = d1.getMinutes(),
+    second = d1.getSeconds();
+  dates.forEach((element) => {
+    const list1 = element.innerHTML.split("/");
+    const [postDate, postMonth, rest] = list1;
+    const list2 = rest.split(" ");
+    const [postYear, rest2] = list2;
+    const list3 = rest2.split(":");
+    const [postHour, postMinute, postSecond] = list3;
+    if (year != postYear) element.innerHTML = `${year - postYear}years ago`;
+    else if (month != postMonth)
+      element.innerHTML = `${month - postMonth} month${
+        month - postMonth > 1 ? "s" : ""
+      } ago`;
+    else if (day != postDate)
+      element.innerHTML = `${day - postDate} day${
+        day - postDate > 1 ? "s" : ""
+      } ago`;
+    else if (hour != postHour)
+      element.innerHTML = `${hour - postHour} hour${
+        hour - postHour > 1 ? "s" : ""
+      } ago`;
+    else if (minute != postMinute)
+      element.innerHTML = `${minute - postMinute} min${
+        minute - postMinute > 1 ? "s" : ""
+      } ago`;
+    else if (second != postSecond)
+      element.innerHTML = `${second - postSecond}s  ago`;
+  });
 };
 
 function getCookie(cname) {
@@ -104,7 +136,8 @@ function orderPosts(e) {
   }
 }
 
-
-if ( window.history.replaceState ) {
-  window.history.replaceState( null, null, window.location.href );
+if (window.history.replaceState) {
+  window.history.replaceState(null, null, window.location.href);
 }
+
+///
